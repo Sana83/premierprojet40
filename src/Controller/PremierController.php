645d608d7f1aad19170ea34ddc8acef0cac0bd45
Sana83 <83754212+Sana83@users.noreply.github.com>
2021-11-30 12:21:39 +0100

    Partie 9 : TRAVAIL AVEC UNE BASE DE DONNEES

---
+++ src/Controller/PremierController.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PremierController {
+    
+    /**
+     * @Route("/index", name="index")
+     */
+    public function index() {
+        return new Response($content = 'Bonjour mon premier contrôleur');
+    }
+}
