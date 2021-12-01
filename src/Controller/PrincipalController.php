a0dd018f84e3b2cb8b0e58773cf24a55c9c430ec
Sana83 <83754212+Sana83@users.noreply.github.com>
2021-12-01 10:02:32 +0100

    Partie 10 : LIER DES CLASSES p40

--- src/Controller/PrincipalController.php
+++ src/Controller/PrincipalController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Employe;
 
 class PrincipalController extends AbstractController
 {
@@ -31,7 +33,7 @@ class PrincipalController extends AbstractController
     /**
      * @Route("/message/{departement}/{sexe}", name="message")
      */
-    public function message(int $departement, string $sexe='f'){
+    public function message(int $departement, string $sexe='F') :Response{
         //$date = date('l jS \of F Y h:i:s A');
         
         return$this->render('principal/message.html.twig', array(
@@ -40,5 +42,25 @@ class PrincipalController extends AbstractController
             "sexe"=>$sexe,
             "date"=>date('l jS \of F Y h:i:s A')
         ));
+    }
+    
+    /**
+     * @Route("/employes", name="employes")
+     * @param ManagerRegistry $doctrine
+     */
+    public function afficheEmployes(ManagerRegistry $doctrine) :Response {
+        $employes = $doctrine->getRepository(Employe::class)->findAll();
+        $titre = "Liste des employés";
+        return $this->render('principal/employes.html.twig', compact('titre', 'employes'));
+    }
+    
+    /**
+     * @Route("/employe/{id}", name="unemploye" , requirements={"id":"\d+"})
+     * @param ManagerRegistry $doctrine
+     */
+    public function afficheUnEmploye(ManagerRegistry $doctrine, int $id) :Response {
+        $employe = $doctrine->getRepository(Employe::class)->find($id);
+        $titre = "Employé n° ". $id;
+        return $this->render('principal/unemploye.html.twig', compact('titre', 'employe'));
     }
 }
