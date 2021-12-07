6520a85061dbd5322d6ccab2efe5f228f57f75b6
Sana83 <83754212+Sana83@users.noreply.github.com>
2021-12-07 12:35:44 +0100

    FORMULAIRE DE DEMANDE DE CONTACT

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use App\Form\ContactType;
 
 class ContactController extends AbstractController
 {
